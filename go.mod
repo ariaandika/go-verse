5fdce4292428a2cb701557aa0d1077958cc09975
ariaandika <andikaakun17@gmail.com>
2024-01-10 18:58:13 +0700

    add loggin

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module go-verse/madman
+module go-verse/main
 
 go 1.21.0
--- main.go
+++ main.go
@@ -14,16 +14,21 @@ import (
 var hosts = map[string]interface{}{}
 
 func main() {
-	port := ":8000"
+    port := ":8000"
 
     for _, arg := range os.Environ() {
         env := strings.Split(arg, "=")
         conf := strings.Split(env[1], ":")
         if strings.HasPrefix(env[0], "SERVE") {
             hosts[conf[0]] = http.FileServer(http.Dir(conf[1]))
+            log.Printf("Serving %s", conf[1])
         }
         if strings.HasPrefix(env[0], "PROXY") {
             hosts[conf[0]] = conf[1]
+            log.Printf("Proxying to %s", conf[1])
+        }
+        if env[0] == "PORT" {
+            port = fmt.Sprintf(":%s", env[1])
         }
     }
 
@@ -56,8 +61,10 @@ func main() {
     cert, certOk := os.LookupEnv("CERT")
 
     if keyOk && certOk {
+        log.Printf("listening with tls in http://localhost%s", port)
 		log.Fatal(http.ListenAndServeTLS(port, cert, key, nil))
 	} else {
+        log.Printf("listening in http://localhost%s", port)
 		log.Fatal(http.ListenAndServe(port, nil))
 	}
 }
